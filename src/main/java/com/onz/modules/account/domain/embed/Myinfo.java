07b2ffb65c6cde8fb8f7e33ba7fd3de50df809a4
최지훈 <hunnx27@i-screamkids.com>
2022-07-20 13:51:04 +0900

    add: 관심기관,관심지역 변수용어통일

--- src/main/java/com/onz/modules/account/domain/embed/Myinfo.java
+++ src/main/java/com/onz/modules/account/domain/embed/Myinfo.java
@@ -1,25 +1,22 @@
 package com.onz.modules.account.domain.embed;
 
-import com.onz.modules.account.domain.enums.GubnConverter;
-import com.onz.modules.account.domain.enums.IntrsOrg;
-import com.onz.modules.account.domain.enums.IntrsOrgConverter;
+import com.onz.common.enums.InterestOrg;
+import com.onz.common.enums.InterestOrgConverter;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Convert;
 import javax.persistence.Embeddable;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 
 @Getter
 @Setter
 @Embeddable
 public class Myinfo {
 //    @Enumerated(EnumType.STRING)
-    @Convert(converter = IntrsOrgConverter.class)
-    private IntrsOrg intrsOrg; // 관심 기관
+    @Convert(converter = InterestOrgConverter.class)
+    private InterestOrg interestOrg; // 관심 기관
 
-    private String IntrsZone="12345"; // 관심지역
+    private String interestZone; // 관심지역
 
     private String birthYYYY; // 생년
 
@@ -30,9 +27,9 @@ public class Myinfo {
     public Myinfo() {
     }
 
-    public Myinfo(IntrsOrg intrsOrg, String intrsZone, String birthYYYY, String majorSchool, String majorDepartment) {
-        this.intrsOrg = intrsOrg;
-        this.IntrsZone = intrsZone;
+    public Myinfo(InterestOrg interestOrg, String interestZone, String birthYYYY, String majorSchool, String majorDepartment) {
+        this.interestOrg = interestOrg;
+        this.interestZone = interestZone;
         this.birthYYYY = birthYYYY;
         this.majorSchool = majorSchool;
         this.majorDepartment = majorDepartment;
