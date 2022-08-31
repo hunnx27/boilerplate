641bed46cc9802b340deb6e2440d9d90907c3313
leehosang <leehs@i-screamkids.com>
2022-08-31 17:42:13 +0900

    2022-08-31[Update]
    (+)기관 -> 기관코드로 검색시 계산함수 적용해서 반환

--- src/main/java/com/onz/modules/review/web/dto/CompanyReviewListResponseDto.java
+++ src/main/java/com/onz/modules/review/web/dto/CompanyReviewListResponseDto.java
@@ -44,6 +44,10 @@ public class CompanyReviewListResponseDto {
     private String itemC3Name;
     private String itemD1Name;
     private String itemD2Name;
+    private double starJumsuTotalAvg;
+    private int starJumsuWorkload;
+    private int starJumsuJobSatisfaction;
+    private int starJumsuWorkAtmosphere;
 
     public CompanyReviewListResponseDto(CompanyReview companyReview){
         this.id=companyReview.getId();
@@ -56,23 +60,26 @@ public class CompanyReviewListResponseDto {
         this.txt=companyReview.getTxt();
         this.createdAt=companyReview.getCreatedAt();
         this.likeCode=companyReview.getLikeCode().name();
-        this.itemB1=companyReview.getItemB1().name();
-        this.itemB2=companyReview.getItemB2().name();
-        this.itemB3=companyReview.getItemB3().name();
-        this.itemC1=companyReview.getItemC1().name();
-        this.itemC2=companyReview.getItemC2().name();
-        this.itemC3=companyReview.getItemC3().name();
-        this.itemD1=companyReview.getItemD1().name();
-        this.itemD2=companyReview.getItemD2().name();
-        this.likeCodeName=companyReview.getLikeCode().getName();
-        this.itemB1Name=companyReview.getItemB1().getName();
-        this.itemB2Name=companyReview.getItemB2().getName();
-        this.itemB3Name=companyReview.getItemB3().getName();
-        this.itemC1Name=companyReview.getItemC1().getName();
-        this.itemC2Name=companyReview.getItemC2().getName();
-        this.itemC3Name=companyReview.getItemC3().getName();
-        this.itemD1Name=companyReview.getItemD1().getName();
-        this.itemD2Name=companyReview.getItemD2().getName();
-
+//        this.itemB1=companyReview.getItemB1().name();
+//        this.itemB2=companyReview.getItemB2().name();
+//        this.itemB3=companyReview.getItemB3().name();
+//        this.itemC1=companyReview.getItemC1().name();
+//        this.itemC2=companyReview.getItemC2().name();
+//        this.itemC3=companyReview.getItemC3().name();
+//        this.itemD1=companyReview.getItemD1().name();
+//        this.itemD2=companyReview.getItemD2().name();
+//        this.likeCodeName=companyReview.getLikeCode().getName();
+//        this.itemB1Name=companyReview.getItemB1().getName();
+//        this.itemB2Name=companyReview.getItemB2().getName();
+//        this.itemB3Name=companyReview.getItemB3().getName();
+//        this.itemC1Name=companyReview.getItemC1().getName();
+//        this.itemC2Name=companyReview.getItemC2().getName();
+//        this.itemC3Name=companyReview.getItemC3().getName();
+//        this.itemD1Name=companyReview.getItemD1().getName();
+//        this.itemD2Name=companyReview.getItemD2().getName();
+        this.starJumsuTotalAvg = companyReview.getStarJumsuTotalAvg();
+        this.starJumsuWorkload = companyReview.getStarJumsuWorkload();
+        this.starJumsuJobSatisfaction =  companyReview.getStarJumsuJobSatisfaction();
+        this.starJumsuWorkAtmosphere = companyReview.getStarJumsuWorkAtmosphere();
+        }
     }
-}
