57f1b650c0d6deb5a7fffbf6f86b7d31a71baef1
leehosang <leehs@i-screamkids.com>
2022-09-05 17:51:25 +0900

    fix: 면접리뷰 response 수정
    (-) "-" 제거

--- src/main/java/com/onz/modules/review/web/dto/InterviewReviewDetailResponseDto.java
+++ src/main/java/com/onz/modules/review/web/dto/InterviewReviewDetailResponseDto.java
@@ -24,22 +24,22 @@ public class InterviewReviewDetailResponseDto {
             itemTest1 = new ArrayList<>();
             for (int i=0; i<interviewReview.getItem_1().length();i++) {
                 if (interviewReview.getItem_1().charAt(i) == '1') {
-                    itemTest1.add("-제작한 교구보여주기");
+                    itemTest1.add("제작한 교구보여주기");
                 }
                 if (interviewReview.getItem_1().charAt(i) == '2') {
-                    itemTest1.add("-피아노 연주하기");
+                    itemTest1.add("피아노 연주하기");
                 }
                 if (interviewReview.getItem_1().charAt(i) == '3') {
-                    itemTest1.add("-수업하기");
+                    itemTest1.add("수업하기");
                 }
             }
         }
         this.itemTest2 = interviewReview.getItem_2()!=null&&"Y".equals(interviewReview.getItem_2().name())? "있어요" : "없어요";
         this.itemTest3 = interviewReview.getItem_3()!=null&&"Y".equals(interviewReview.getItem_3().name())? "있어요" : "없어요";
         switch(interviewReview.getItem_5()!=null?interviewReview.getItem_5():"999"){
-            case "1": this.itemMood = "-여유";break;
-            case "2": this.itemMood = "-편안";break;
-            case "3": this.itemMood = "-긴장";break;
+            case "1": this.itemMood = "여유";break;
+            case "2": this.itemMood = "편안";break;
+            case "3": this.itemMood = "긴장";break;
             default: this.itemMood = "-";break;
         }
         this.workExp = interviewReview.getWorkExp();
